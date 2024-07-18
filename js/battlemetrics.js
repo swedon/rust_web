00f18abe782b616a5b77812f11452c3ac338de3e
Limmek <jimaugustsson@gmail.com>
2024-07-18 05:37:31 +0200

    initial upload of the old website

---
+++ js/battlemetrics.js
@@ -0,0 +1,69 @@
+// Function to fetch data from the data.json file and update server information
+async function fetchServerInfo() {
+    try {
+        const response = await fetch('https://raw.githubusercontent.com/swedon/rust_web/main/data/server_info.json');
+        if (response.ok) {
+            const data = await response.json();
+            // Extract and update the content with the desired information
+            const serverInfo = data.data.attributes;
+
+            // Update the server name
+            const serverName = document.getElementById('server-name')
+            serverName.textContent = serverInfo.name;
+            serverName.classList.remove("server-offline");
+
+            // Update the server name in the modal window.
+            const modal_serverName = document.getElementById('modal-server-name')
+            modal_serverName.textContent = serverInfo.name;
+            modal_serverName.classList.remove("server-offline");
+
+            // Update server info
+            document.getElementById('server-info').textContent = serverInfo.details.map;
+            document.getElementById('modal-server-info').textContent = `${serverInfo.details.map} - ${serverInfo.players}/${serverInfo.maxPlayers}`;
+
+            // Update the server description with replaced characters
+            document.getElementById('modal-server-description').innerHTML = replaceNewlinesAndTabs(serverInfo.details.rust_description);
+
+            // Update the player count
+            document.getElementById('player-count').innerHTML = `${serverInfo.players}<b>/</b><span class="player-count-max">${serverInfo.maxPlayers}</span>`;
+
+            // Update the progress bar
+            const playerProgress = (serverInfo.players / serverInfo.maxPlayers) * 100;
+            const playerProgressBar = document.getElementById('player-count-progressbar')
+            playerProgressBar.style.width = `${playerProgress}%`;
+            playerProgressBar.setAttribute('aria-valuenow', playerProgress);
+
+            // Update steam connect buttons with server ip and port
+            const steamConnect = `location.href='steam://connect/${serverInfo.ip}:${serverInfo.port}'`
+            document.getElementById('steam-connect').setAttribute('onclick', steamConnect);
+            document.getElementById('modal-steam-connect').setAttribute('onclick', steamConnect);
+            document.getElementById('steam-connect').classList.remove('disabled');
+            document.getElementById('modal-steam-connect').classList.remove('disabled');
+
+            document.getElementById('player-count-progress').style.display = 'block';
+            document.getElementById('server-tags').style.display = 'block';
+            document.getElementById('server-players').style.display = 'block';
+
+        } else {
+            console.error('Error fetching data:', response.status, response.statusText);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Helper function to replace \n with <br> and \t with &emsp (tabb);
+// This is a &nbsp; regular space.
+// This is a &ensp; two spaces gap.
+// This is a &emsp; four spaces gap.
+function replaceNewlinesAndTabs(text) {
+    return text
+        .replace(/\n/g, '<br>')
+        .replace(/\\t/g, '&emsp;');
+}
+
+// Called wehn page is loaded
+document.addEventListener('DOMContentLoaded', function () {
+    // Call the function to fetch and update server information
+    fetchServerInfo();
+});
---
+++ js/script.js
@@ -0,0 +1,102 @@
+// Animation scripts
+// Get all the member container elements
+const memberContainers = document.querySelectorAll('.member-container');
+
+// Loop through each member container and add the animation on mouseover
+memberContainers.forEach(memberContainer => {
+    // Remove the animation on mouseout
+    memberContainer.addEventListener('mouseout', () => {
+        memberContainer.classList.remove('animate__heartBeat');
+    });
+
+    memberContainer.addEventListener('mouseover', () => {
+        memberContainer.classList.add('animate__heartBeat');
+    });
+});
+
+// Set active link in navigation
+// Get the container element
+var btnContainer = document.getElementById("navbarMain");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("nav-link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+
+// Change the background video to a random source
+const backgroundVideo = document.getElementById("background-video");
+const sourceElements = backgroundVideo.querySelectorAll("source");
+const videoSources = Array.from(sourceElements).map(source => source.getAttribute("src"));
+shuffleArray(videoSources);
+
+let currentIndex = 0;
+let isPlaying = false;
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function playNextVideo() {
+    if (isPlaying) {
+        currentIndex = (currentIndex + 1) % videoSources.length;
+    }
+    backgroundVideo.src = videoSources[currentIndex];
+    backgroundVideo.load();
+    backgroundVideo.play().catch(error => {
+        // Autoplay was prevented, you can handle this situation here.
+        console.error("Autoplay prevented:", error);
+    });
+    isPlaying = true;
+}
+
+backgroundVideo.addEventListener("ended", playNextVideo);
+if (!isPlaying) {
+        playNextVideo();
+    }
+
+
+// Change server header image src when a element is hovered
+function changeImage(serverDiv, hover) {
+    const logoImage = serverDiv.querySelector('.header-logo');
+    if (hover) {
+        logoImage.src = './images/logo_white_active.webp';
+    } else {
+        logoImage.src = './images/logo_white.webp';
+    }
+}
+
+// Add server tags
+async function fetchServerTags() {
+    try {
+        const response = await fetch('../data/server_tags.json');
+        if (response.ok) {
+            const data = await response.json();
+            document.getElementById('server-tags').innerHTML = "";
+            data.forEach(tag => {
+                document.getElementById('server-tags').innerHTML += `<span class="badge text-bg-primary">${tag}</span>`;
+            })
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+// Called wehn page is loaded
+document.addEventListener('DOMContentLoaded', function () {
+    // Call the function to fetch and update server information
+    fetchServerTags()
+    if (!isPlaying) {
+        playNextVideo();
+    }
+});
